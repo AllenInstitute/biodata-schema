793097b901938af0d0a92ea4d1d7a051d47f44a5
Saskia de Vries <saskiad@alleninstitute.org>
2026-09-17 20:53:56 -0700

    adding cell line to subjects

--- src/biodata_schema/core/subject.py
+++ src/biodata_schema/core/subject.py
@@ -1,11 +1,11 @@
-"""schema for mostly mouse metadata"""
+"""schema for subject metadata"""
 
 from typing import Literal, Optional
 
 from pydantic import Field, SkipValidation
 
 from biodata_schema.base import DataCoreModel, Discriminated
-from biodata_schema.components.subjects import CalibrationObject, HumanSubject, MouseSubject, NonHumanPrimateSubject
+from biodata_schema.components.subjects import CalibrationObject, CellLine, HumanSubject, MouseSubject, NonHumanPrimateSubject
 
 
 class Subject(DataCoreModel):
@@ -20,7 +20,12 @@ class Subject(DataCoreModel):
         title="Subject ID",
     )
 
-    subject_details: Discriminated[MouseSubject | HumanSubject | NonHumanPrimateSubject | CalibrationObject] = Field(
+    subject_details: Discriminated[MouseSubject |
+                                   HumanSubject |
+                                   NonHumanPrimateSubject |
+                                   CalibrationObject |
+                                   CellLine
+                                   ] = Field(
         ..., title="Subject Details"
     )
 
